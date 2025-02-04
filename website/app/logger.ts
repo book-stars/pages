96a61223fe0b1e0c2e55e90cac66b60541a90d52
bookstars <dev@booksta.rs>
2025-02-04 18:42:34 +0100

    chore: trying pretty again

--- website/app/logger.ts
+++ website/app/logger.ts
@@ -1,30 +1,32 @@
 import pino from "pino";
-
+// import pretty from 'pino-pretty'
+// const stream = pretty({
+//     colorize: true
+// })
 const config = {
     level: import.meta.env.LOG_LEVEL || 'debug',
     transport: {
         targets: [
-
         ]
     }
 
 } as any
 
 // if (import.meta.env.DEV) {
-// config.transport.targets.push({
-//     target: 'pino-pretty',
-//     translateTime: true,
-
-//     options: {
-//         colorize: true,
-//         destination: 1
-//     }
-// })
-// } else {
 config.transport.targets.push({
-    target: 'pino/file',
-    options: { destination: 1 } // this writes to STDOUT
+    target: 'pino-pretty',
+    translateTime: true,
+
+    options: {
+        colorize: true,
+        destination: 1
+    }
 })
+// } else {
+// config.transport.targets.push({
+//     target: 'pino/file',
+//     options: { destination: 1 } // this writes to STDOUT
+// })
 // }
 
 
